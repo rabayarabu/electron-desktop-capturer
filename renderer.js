0b3675e7a89c31905e190c53a7c7fe813b799a62
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-10-28 01:43:37 +0600

    Added micromodal

--- renderer.js
+++ renderer.js
@@ -1,3 +1,63 @@
+/**
+ * This file is loaded via the <script> tag in the index.html file and will
+ * be executed in the renderer process for that window. No Node.js APIs are
+ * available in this process because `nodeIntegration` is turned off and
+ * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
+ * to expose Node.js functionality from the main process.
+ */
+const main = window.electronApi.main;
+
+const video = document.querySelector('#stream');
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+const openSettingsBtn = document.querySelector('#system-preferences');
+const screenPicker = document.querySelector('#electron-screen-picker');
+
+const displayMediaOptions = {
+	video: {
+		displaySurface: "window",
+	},
+	audio: false,
+};
+
+navigator.mediaDevices.getDisplayMedia = getDisplayMedia;
+
+// Set event listeners for the start, stop and openSettings buttons
+startBtn.addEventListener("click", startCapture, false);
+stopBtn.addEventListener("click", stopCapture, false);
+openSettingsBtn.addEventListener("click", openPreferences, false);
+
+async function startCapture() {
+	try {
+		video.srcObject = await navigator.mediaDevices.getDisplayMedia(
+			displayMediaOptions
+		);
+		startBtn.classList = 'hidden';
+		stopBtn.classList = '';
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+function stopCapture(evt) {
+	if (!video.srcObject) return;
+	
+	let tracks = video.srcObject.getTracks();
+
+	tracks.forEach((track) => track.stop());
+	video.srcObject = null;
+	startBtn.classList = '';
+	stopBtn.classList = 'hidden';
+}
+
+function openPreferences() {
+	main.openScreenSecurity();
+}
+
+let screenPickerOptions = {
+	system_preferences: false
+};
+
 function getDisplayMedia() {
 	if (main.isOSX()) {
 		screenPickerOptions.system_preferences = true;
@@ -38,4 +98,39 @@ function getDisplayMedia() {
 			reject(err);
 		}
 	});
+}
+
+function screenPickerShow(sources, onselect) {
+	const list = document.querySelector('#sources');
+	list.innerHTML = '';
+
+	sources.forEach(source => {
+		const item = document.createElement('div');
+		item.classList = '__electron-list';
+
+		const wrapper = document.createElement('div');
+		wrapper.classList = 'thumbnail __electron-screen-thumbnail';
+
+		const thumbnail = document.createElement('img');
+		thumbnail.src = source.thumbnailURL;
+
+		const label = document.createElement('div');
+		label.classList = '__electron-screen-name';
+		label.innerText = source.name;
+
+		wrapper.append(thumbnail);
+		wrapper.append(label);
+		item.append(wrapper);
+		item.onclick = () => {
+			onselect(source.id);
+			MicroModal.close('electron-screen-picker');
+		};
+		list.append(item);
+	});
+	
+	if (!screenPickerOptions.system_preferences) {
+		openSettingsBtn.classList = 'hidden';
+	}
+
+	MicroModal.show('electron-screen-picker');
 }
