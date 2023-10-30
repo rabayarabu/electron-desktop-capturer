9a5bb5f3ba0150296d8cbf5ce58e8c7498f9a1a5
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-10-30 20:48:44 +0600

    Add UI version-1

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 1500,
-        height: 1000,
+        height: 800,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             devTools: true,
