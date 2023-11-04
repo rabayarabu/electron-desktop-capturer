e2f7822b72e9b79ff3eaabae633da2de8afeefe5
Rabaya Rabu <70410637+rabayarabu@users.noreply.github.com>
2023-11-04 19:56:57 +0600

    Edit index & renderer files

--- renderer.js
+++ renderer.js
@@ -21,81 +21,19 @@ const displayMediaOptions = {
 	video: {
 		displaySurface: "window",
 	},
-	audio: false,
+	audio: true,
 };
 
-navigator.mediaDevices.getDisplayMedia = getDisplayMedia;
+var screenPickerOptions = {
+	system_preferences: false
+};
+var mediaRecorder = null;
+var recordedCunks = [];
+
+// navigator.mediaDevices.getDisplayMedia = getDisplayMedia;
 // start recording
-if (navigator.mediaDevices) {
-	console.log("getUserMedia supported.");
-  
-	const constraints = { audio: true };
-	let chunks = [];
-  
-	navigator.mediaDevices
-	  .getUserMedia(constraints)
-	  .then((stream) => {
-		const mediaRecorder = new MediaRecorder(stream);
-  
-		startBtn.onclick = () => {
-		  mediaRecorder.start();
-		  console.log(mediaRecorder.state);
-		  console.log("recorder started");
-		  startBtn.style.background = "red";
-		  startBtn.style.color = "black";
-		};
-  
-		stop.onclick = () => {
-		  mediaRecorder.stop();
-		  console.log(mediaRecorder.state);
-		  console.log("recorder stopped");
-		  startBtn.style.background = "";
-		  startBtn.style.color = "";
-		};
-  
-		mediaRecorder.onstop = (e) => {
-		  console.log("data available after MediaRecorder.stop() called.");
-  
-		  const clipName = prompt("Enter a name for your sound clip");
-  
-		  const clipContainer = document.createElement("article");
-		  const clipLabel = document.createElement("p");
-		  const audio = document.createElement("audio");
-		  const deleteButton = document.createElement("button");
-		  const mainContainer = document.querySelector("body");
-  
-		  clipContainer.classList.add("clip");
-		  audio.setAttribute("controls", "");
-		  deleteButton.textContent = "Delete";
-		  clipLabel.textContent = clipName;
-  
-		  clipContainer.appendChild(audio);
-		  clipContainer.appendChild(clipLabel);
-		  clipContainer.appendChild(deleteButton);
-		  mainContainer.appendChild(clipContainer);
-  
-		  audio.controls = true;
-		  const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-		  chunks = [];
-		  const audioURL = URL.createObjectURL(blob);
-		  audio.src = audioURL;
-		  console.log("recorder stopped");
-  
-		  deleteButton.onclick = (e) => {
-			const evtTgt = e.target;
-			evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-		  };
-		};
-  
-		mediaRecorder.ondataavailable = (e) => {
-		  chunks.push(e.data);
-		};
-	  })
-	  .catch((err) => {
-		console.error(`The following error occurred: ${err}`);
-	  });
-  }
-  
+
+
 // Set event listeners for the start, stop and openSettings buttons
 startBtn.addEventListener("click", startCapture, false);
 stopBtn.addEventListener("click", stopCapture, false);
@@ -103,75 +41,98 @@ openSettingsBtn.addEventListener("click", openPreferences, false);
 
 async function startCapture() {
 	try {
-		video.srcObject = await navigator.mediaDevices.getDisplayMedia(
-			displayMediaOptions
-		);
 		startBtn.classList = 'hidden';
 		stopBtn.classList = '';
+		getDisplayMedia();
 	} catch (err) {
 		console.error(err);
 	}
 }
 
 function stopCapture(evt) {
-	if (!video.srcObject) return;
-	
-	let tracks = video.srcObject.getTracks();
-
-	tracks.forEach((track) => track.stop());
-	video.srcObject = null;
 	startBtn.classList = '';
 	stopBtn.classList = '';
+	if (mediaRecorder !== null) {
+		mediaRecorder.stop();
+	}
 }
 
 function openPreferences() {
 	main.openScreenSecurity();
 }
 
-let screenPickerOptions = {
-	system_preferences: false
-};
-
-function getDisplayMedia() {
+async function getDisplayMedia() {
 	if (main.isOSX()) {
 		screenPickerOptions.system_preferences = true;
 	}
 
-	return new Promise(async (resolve, reject) => {
-		let has_access = await main.getScreenAccess();
-		if (!has_access) {
-			return reject('none');
-		}
+	let has_access = await main.getScreenAccess();
+	if (!has_access) {
+		return reject('none');
+	}
 
-		try {
-			const sources = await main.getScreenSources();
-			screenPickerShow(sources, async (id) => {
-				try {
-					const source = sources.find(source => source.id === id);
-					if (!source) {
-						return reject('none');
-					}
+	try {
+		const sources = await main.getScreenSources();
+		screenPickerShow(sources, async (id) => {
+			try {
+				const source = sources.find(source => source.id === id);
+				if (!source) {
+					return reject('none');
+				}
 
-					const stream = await window.navigator.mediaDevices.getUserMedia({
-						audio: false,
-						video: {
-							mandatory: {
-								chromeMediaSource: 'desktop',
-								chromeMediaSourceId: source.id
-							}
+				const stream = await window.navigator.mediaDevices.getUserMedia({
+					audio: true,
+					video: {
+						mandatory: {
+							chromeMediaSource: 'desktop',
+							chromeMediaSourceId: source.id
 						}
-					});
-					resolve(stream);
+					}
+				});
+
+				if (stream) {
+					console.log("getUserMedia supported.", stream);
+					mediaRecorder = new MediaRecorder(stream);
+					mediaRecorder.onstart = () => {
+						console.log("recorder started");
+						startBtn.style.background = "red";
+						startBtn.style.color = "black";
+					}
+					mediaRecorder.ondataavailable = (e) => {
+						const { data } = e;
+						if (data.size > 0) {
+							recordedCunks.push(e.data);
+						}
+					};
+					mediaRecorder.onstop = (e) => {
+						if (recordedCunks.length > 0) {
+							const videoUrl = URL.createObjectURL(new Blob(recordedCunks, { type: 'video/mp4' }));
+							downloadFile(videoUrl, 'recording');
+						}
+
+						console.log("data available after MediaRecorder.stop() called.");
+
+					};
+					mediaRecorder.start();
 				}
-				catch (err) {
-					reject(err);
-				}
-			}, {});
-		}
-		catch (err) {
-			reject(err);
-		}
-	});
+			}
+			catch (err) {
+				reject(err);
+			}
+		}, {});
+	}
+	catch (err) {
+		reject(err);
+	}
+}
+
+function downloadFile(uri, name) {
+	let link = document.createElement('a');
+	link.download = name;
+	link.href = uri;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
 }
 
 function screenPickerShow(sources, onselect) {
@@ -201,10 +162,14 @@ function screenPickerShow(sources, onselect) {
 		};
 		list.append(item);
 	});
-	
+
 	if (!screenPickerOptions.system_preferences) {
 		openSettingsBtn.classList = 'hidden';
 	}
 
 	MicroModal.show('electron-screen-picker');
 }
+
+
+/* ---------------------------- UI functions ---------------------------------------------- */
+
